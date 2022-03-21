e51aa19641e6f44059f2b3cc57e0a6b07b657893
ThanhSangNguyen <thanhsang.nguyen.vtp@gmail.com>
2022-03-21 18:01:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/bottomnav/BottomNav.java
@@ -0,0 +1,74 @@
+package com.example.bottomnav;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class BottomNav extends AppCompatActivity {
+BottomNavigationView navigationView;
+ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bottom_nav);
+        viewPager = findViewById(R.id.viewpager);
+        navigationView = findViewById(R.id.bottom_nav);
+        setUpViewPager();
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.action_home:
+                        viewPager.setCurrentItem(0);
+                        break;
+                    case R.id.action_favorite:
+                        viewPager.setCurrentItem(1);
+                        break;
+                    case R.id.action_bookmark:
+                        viewPager.setCurrentItem(2);
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+    private void setUpViewPager(){
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPager.setAdapter(viewPagerAdapter);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position){
+                    case 0:
+                        navigationView.getMenu().findItem(R.id.action_home).setChecked(true);
+                        break;
+                    case 1:
+                        navigationView.getMenu().findItem(R.id.action_favorite).setChecked(true);
+                        break;
+                    case 2:
+                        navigationView.getMenu().findItem(R.id.action_bookmark).setChecked(true);
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+}
