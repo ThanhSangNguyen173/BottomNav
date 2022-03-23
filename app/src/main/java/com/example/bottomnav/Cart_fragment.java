7905ba135abf2c149a7c6f84c95d0fc9afe6e7ac
ThanhSangNguyen <thanhsang.nguyen.vtp@gmail.com>
2022-03-23 13:16:32 +0700

    Commit Day 8 - Cart

--- app/src/main/java/com/example/bottomnav/Cart_fragment.java
+++ app/src/main/java/com/example/bottomnav/Cart_fragment.java
@@ -4,16 +4,65 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Cart_fragment extends Fragment {
+    private RecyclerView rcvProduct;
+    private View mView;
+    private BottomNav bottomNav;
+    private ProductAdapter productAdapter;
+
+    public Cart_fragment(){
+
+    }
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.cart_fragment,container,false);
-        return view;
+        mView = inflater.inflate(R.layout.product_fragment,container,false);
+        bottomNav = (BottomNav) getActivity();
+        rcvProduct = mView.findViewById(R.id.rcv_product);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(bottomNav);
+        rcvProduct.setLayoutManager(linearLayoutManager);
+        productAdapter = new ProductAdapter();
+        productAdapter.setData(getListProduct(), new ProductAdapter.IClickAddtoCartListener() {
+            @Override
+            public void onClickAddtoCart(ImageView imgAddtoCart, Product product) {
+                AnimationUtil.translateAnimation(bottomNav.getViewAnimation(), imgAddtoCart, bottomNav.getViewEndAnimation(), new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+            }
+        });
+        rcvProduct.setAdapter(productAdapter);
+
+        return mView;
+    }
+
+    private List<Product> getListProduct() {
+        List<Product> list = new ArrayList<>();
+
+        return list;
     }
 }
